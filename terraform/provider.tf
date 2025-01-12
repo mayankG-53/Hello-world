5e50b09a219dfbbe38b9d8691b400cc29c5f3695
mayankG-53 <mayank.gorakh91@gmail.com>
2025-01-12 16:47:58 +0000

    27

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,6 @@
-terraform {
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.15.0"
-    }
-  }
-  
 provider "aws" {
   region = var.aws_region
 }
 
-  }
+  
