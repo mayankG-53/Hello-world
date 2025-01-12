ab02af0e4a5e2f314ff3acb2c7d8d9cb642c101e
mayankG-53 <mayank.gorakh91@gmail.com>
2025-01-12 16:34:56 +0000

    25

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
-
-provider "aws" {
-  region = "us-west-2"
-}
 
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
