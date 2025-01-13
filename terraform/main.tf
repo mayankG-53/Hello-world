65dc5586ea4423d9108641b558cfda8de5dc5d5a
mayankG-53 <mayank.gorakh91@gmail.com>
2025-01-13 08:41:35 +0000

    55

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,8 @@ resource "aws_subnet" "subnet1" {
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-1"
+    kubernetes.io/cluster/my-cluster = "shared"
+    kubernetes.io/role/elb = "1"
   }
 }
 
@@ -21,6 +23,8 @@ resource "aws_subnet" "subnet2" {
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet-2"
+    kubernetes.io/cluster/my-cluster = "shared"
+    kubernetes.io/role/elb = "1"
   }
 }
 
@@ -31,6 +35,8 @@ resource "aws_subnet" "subnet3" {
   availability_zone = "us-east-1a"
   tags = {
     Name = "private-subnet-1"
+    kubernetes.io/cluster/my-cluster = "shared"
+    kubernetes.io/role/internal-elb = "1"
   }
 }
 
@@ -40,6 +46,8 @@ resource "aws_subnet" "subnet4" {
   availability_zone = "us-east-1b"
   tags = {
     Name = "private-subnet-2"
+    kubernetes.io/cluster/my-cluster = "shared"
+    kubernetes.io/role/internal-elb = "1"
   }
 }
 
