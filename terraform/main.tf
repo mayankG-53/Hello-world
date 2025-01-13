56531c5608828813dc04b2ed49bb4ed5e10bb182
mayankG-53 <mayank.gorakh91@gmail.com>
2025-01-13 16:30:03 +0000

    61

--- terraform/main.tf
+++ terraform/main.tf
@@ -201,23 +201,16 @@ resource "aws_security_group" "eks_security_group" {
   vpc_id      = aws_vpc.main.id 
 
   ingress {
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["10.0.0.0/16"]  # VPC CIDR block or specific internal ranges
-}
-
-ingress {
-  from_port   = 1025
-  to_port     = 65535
-  protocol    = "tcp"
-  cidr_blocks = ["10.0.0.0/16"]  # For internal communication between nodes
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16", ]  # VPC CIDR block or specific internal ranges
 }
 
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]  # Allow egress traffic to anywhere
   }
 }
