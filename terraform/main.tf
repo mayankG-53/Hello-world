43c55f04652f019352fe0a83677ced05f6761e2d
mayankG-53 <mayank.gorakh91@gmail.com>
2025-01-14 21:51:49 +0000

    100

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,13 +14,9 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "mayank-eks-${random_string.suffix.result}"
+  cluster_name = "mayank-eks-1"
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
